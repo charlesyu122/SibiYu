db869b3252a2f041832c32fc62377306903a123c
Charles Yu <charlesyu122@gmail.com>
2012-11-28 23:12:31 +0800

    Added QuickAction function

--- src/com/actone/sibij/yuc/act01/ContactsActivity.java
+++ src/com/actone/sibij/yuc/act01/ContactsActivity.java
@@ -3,10 +3,15 @@ package com.actone.sibij.yuc.act01;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.actone.quickaction.ActionItem;
+import com.actone.quickaction.QuickAction;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 import android.app.ListActivity;
 import android.database.Cursor;
@@ -14,21 +19,25 @@ import android.database.Cursor;
 public class ContactsActivity extends ListActivity {
 
 	private ContactDataSource datasource;
+	Contact selectedContact;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         
-        // Initialize datasource
+        // Initialize attributes
 		datasource = new ContactDataSource(this);
 		datasource.open();
+		selectedContact = new Contact();
 		
         setup();
     }
     
     private void setup(){
-    	// Setup view
+    	// Setup views
+    	TextView tvTitle = (TextView)findViewById(R.id.tvListHeaderTitle);
+    	tvTitle.setText("Contacts");
     	TextView tvNoContact = (TextView)findViewById(R.id.tvNoContacts);
     	
     	// Retrieve contents from Contact Application
@@ -52,7 +61,42 @@ public class ContactsActivity extends ListActivity {
     	}
     	
     	// Set up Quick Action
+    	ActionItem editAction = new ActionItem();
+    	editAction.setIcon(getResources().getDrawable(R.drawable.ic_add));
+    	editAction.setTitle("Edit");
+    	ActionItem deleteAction = new ActionItem();
+    	deleteAction.setIcon(getResources().getDrawable(R.drawable.ic_accept));
+    	deleteAction.setTitle("Delete");
     	
+    	final QuickAction myQA = new QuickAction(this);
+    	myQA.addActionItem(editAction);
+    	myQA.addActionItem(deleteAction);
+    	
+    	getListView().setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View v, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				selectedContact = (Contact) v.getTag();
+				myQA.show(v);
+			}
+		});
+    	
+    	myQA.setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {
+			
+			@Override
+			public void onItemClick(QuickAction source, int pos, int actionId) {
+				// TODO Auto-generated method stub
+				if(pos == 0){
+					// Edit contact
+					
+				}else if(pos == 1){
+					// Delete contact
+					
+				}
+			}
+		});
     }
 
 	@SuppressWarnings("deprecation")
